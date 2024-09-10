43ae3aa54aa9c081cf1287915a01f420e36fbc3d
Johnahan-web <j.tuckerjrmt@gmail.com>
2024-09-10 11:49:04 -0400

    My Second Commit

---
+++ Block07/node.js
@@ -0,0 +1,10 @@
+console.log ("Lock Combination")
+let text =" You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+//Variables for the combination
+const ten = 5+5;
+const forty = 30+10;
+const thirtynine = 38+1;
+//Pop up Alert
+alert(
+     `The combination for the lock is ${ten} -${forty} - ${thirtynine}`
+    );
